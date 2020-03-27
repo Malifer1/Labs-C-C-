58a6ca32bf48a448dec728252ce1c21cee8299a1
Malifer1 <Malifer1@users.noreply.github.com>
2020-03-27 11:57:48 +0300

    Update RE_lab3(a).cpp

--- RE_lab3(a).cpp
+++ RE_lab3(a).cpp
@@ -40,8 +40,7 @@ char* GetStr(void)
 			free(string);
 			return NULL;
 		}
-	} 
-	while (n > 0);
+	} while (n > 0);
 	return string;
 }
 
@@ -62,56 +61,43 @@ char* SkipWord(char* s)
 char* BitOfTruth(char* s)
 {
 	int i,count_sum;
-	int* num;
-	int* count;
-	count=(int*)calloc(1000,sizeof(int));
-	num=(int*)calloc(1000,sizeof(int));
+	int num,count;
 	char bit_0[3]=" 0";
 	char bit_1[3]=" 1";
 	char* s1;
-	char** s0;
-	s0=(char**)calloc(1000,sizeof(char*));
-	s0[i]=(char*)malloc(sizeof(char));
-	i=0;
+	s1=(char*)malloc(sizeof(char));
 	count_sum=0;
 	s1=s;
 	for (i=0;s!='\0';i++)
 	{
-		if(i==0)
-			s0[i]=s;
-		else
-		{
-			s=SkipWord(s);
-			s=SkipSpace(s);
-			s0[i]=s;
-			if(strcspn(s," ")==0)
+		s=SkipSpace(s);	
+		if(strcspn(s," ")==0)
 				break;
-		}
-		num[i]=atoi(s0[i]);
-		while(num[i]!=0)
+		num=atoi(s);
+		while(num!=0)
 		{
-			count[i]=num[i]%10+count[i];
-			num[i]=num[i]/10;
+			count=num%10+count;
+			num=num/10;
 		}
+		count_sum=count_sum+count;
+		count=0;
+		s=SkipWord(s);
 	}
-	for(i=0;count[i]!='\0';i++)
-		count_sum=count_sum+count[i];
 	if (count_sum%2==1)
 		s1=strcat(s1,bit_1);
 	else
 		s1=strcat(s1,bit_0);
-	printf("%s",s1);
+	printf("%s\n",s1);
+	free(s1);
 	return 0;
 }
 
-int main()
+int main(void)
 {
 	int i,j,k;
 	bool flag,flag1;
 	char* s1;
-	char* s2;
 	s1=(char*)malloc(sizeof(char));
-	s2=(char*)malloc(sizeof(char));
 	flag=true;
 	flag1=true;
 	k=0;
@@ -119,17 +105,14 @@ int main()
 	{
 		printf("Enter the string\n");
 		s1=GetStr();
-		s1=SkipSpace(s1);
 		if ((atoi(s1)==0)&&(strcspn(s1,"0")==strlen(s1)))
 			flag1=false;
-		printf("%s\n",s1);
 		printf("Have u done with entering strings? Press 0 to finish or 1 to continue\n");
 		scanf("%d",&flag);
-	//	printf("%s\n",strchr(s1,k));
+		scanf("%*c");
 		if(flag1)
 			BitOfTruth(s1);
 		flag1=true;
-		
 	}
-	
+	free(s1);
 }
